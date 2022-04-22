0050f82b54d32bb0e31c10e6eba21890c148607b
aditya-delloite <aditya-delloite@users.noreply.github.com>
2022-04-22 14:46:41 +0530

    Code-Repushed

--- Automation_ClientBilling/src/test/javaa/TestFiles/RunTests.java
+++ Automation_ClientBilling/src/test/javaa/TestFiles/RunTests.java
@@ -1,7 +1,20 @@
 package javaa.TestFiles;
 
+import javaa.Utilities.BaseUtilities;
+import org.testng.annotations.BeforeClass;
+
 public class RunTests {
 
+    @BeforeClass
+    public void SetUp(){         //Here inside getLogger it accepts class name as a parameter
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\adityakumar3\\Downloads\\chromedriver.exe");
+        driver = BaseUtilities.initialSetup()
+        driver.get(BaseUtilities.ur);
+        driver.manage().window().maximize();
+
+        log.info("Browser Launched");
+    }
 
 
 
